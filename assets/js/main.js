b4deeaf4f68acb87e8e403cb0cf8d7f2821b7490
vismodev <vismodeb2000@gmail.com>
2024-03-14 11:06:42 +0600

    project done

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    // Nav Toggle
+    $('#toggleNav-icon').click(function(){
+      $(this).toggleClass('open');
+      $('.menu').slideToggle();
+    });
+  
+  
+  });
